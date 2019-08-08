b4308c90b50a9cde18a96f53a37a4cc7a77591d9
Alec Hall <alecnhall@gmail.com>
2019-08-08 01:39:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,37 @@
+require "pry"
+
+# Inputs - array of hashes
+
+# Steps
+  # turn array into hash
+  # add counts to hash
+  # if items are same reduce them to same value while increasing counts
+  # Return updated hash
+# Outputs - hash with count added
+
+
 def consolidate_cart(cart)
-  # code here
+  consolidated = { }
+  cart.each do |item|
+    # binding.pry
+    if 
+    consolidated[item.keys[0]] = {
+      price:item.values[0][:price],
+      clearance:item.values[0][:clearance],
+      count: 1
+    }
+    return consolidated
+  end
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
 end
 
 def apply_clearance(cart)
-  # code here
+
 end
 
 def checkout(cart, coupons)
-  # code here
+
 end
