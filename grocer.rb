c51dccea40dba08c2710adbb65163ef3b58d0e28
Alec Hall <alecnhall@gmail.com>
2019-08-08 01:50:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,7 +27,12 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/ COUPON"
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
 end
 
 def apply_clearance(cart)
